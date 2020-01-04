a59e1797c1f7ae97b8b19f87873bc6711efc8b6e
Soumya-Chinnur <soumyachinnur@gmail.com>
2020-01-04 11:35:35 +0530

    [Soumya] refactor.modified fundoo dashboard programs and pushed into remote

--- fundookeep/src/components/dashboard/dashboard.js
+++ fundookeep/src/components/dashboard/dashboard.js
@@ -1,7 +1,7 @@
 
 export default { //standard syntax for instantiating an object that has been defined.
   name: 'dashboard',
-  components: {},
+  components: {},//They help you extend basic HTML elements to encapsulate reusable code
   props: [],
   data () {
     return {
