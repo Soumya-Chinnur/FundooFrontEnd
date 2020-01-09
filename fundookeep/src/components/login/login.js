117918f2dc849419f7dc498dcd735315482689e7
Soumya-Chinnur <soumyachinnur@gmail.com>
2020-01-09 18:10:52 +0530

    [Soumya] refactor.modified loginpassword program and pushed into remote

--- fundookeep/src/components/login/login.js
+++ fundookeep/src/components/login/login.js
@@ -1,8 +1,8 @@
 import login from "../services/userServices"
-export default {
+export default { //syntax for instantiating an object that has been defined
   name: 'login',
-  components: {},
-  props: [],
+  components: {},//logical entities of code that contain functionality
+  props: [], //Props are how you pass data from a parent component down to a child component
   data () {
     return {
       loading: false,
@@ -14,10 +14,10 @@ export default {
 
   
   },
-  computed: {
+  computed: { //A computed property is used to declaratively describe a value that depends on other values
 
   },
-  mounted () {
+  mounted () {   //mounted() is called after DOM has been mounted so you can access the reactive component, templates, and DOM elements and manipulate them
 
   },
   methods: {
@@ -34,10 +34,10 @@ export default {
       })
       // your code to login user
       // this is only for example of loading
-      this.loading = true;
-      setTimeout(() => {
-        this.loading = false;
-      }, 5000);
+      // this.loading = true;
+      // setTimeout(() => {
+      //   this.loading = false;
+      // }, 5000);
     }
   }
 
