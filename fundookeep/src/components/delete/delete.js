e9f7cf81516f837930ed00ed46bcaf7aff81cca6
Soumya-Chinnur <soumyachinnur@gmail.com>
2020-01-20 14:49:00 +0530

    [Soumya] refactor.modified delete component and pushed into remote

--- fundookeep/src/components/delete/delete.js
+++ fundookeep/src/components/delete/delete.js
@@ -1,29 +1,22 @@
-import noteService from "../services/noteService"
-import displayNote from "../displayNote"
+import noteService from "../services/noteService";
+import displayNote from "../displayNote";
 export default {
-  name: 'delete',
+  name: "delete",
   components: {},
   props: [],
-  data () {
-    return {
-
-    }
+  data() {
+    return {};
   },
-  computed: {displayNote},
-  mounted () {
-    noteService.getNotes().then(res=>{
-      console.log("fsd",res);
+  computed: { displayNote },
+  mounted() {
+    noteService.getNotes().then(res => {
+      console.log("fsd", res);
       for (let index = 0; index < res.data.data.data.length; index++) {
         if (res.data.data.data[index].isDeleted) {
           this.deleteCards.push(res.data.data.data[index]);
         }
       }
-
-    })
+    });
   },
-  methods: {
-
-  }
-}
-
-
+  methods: {}
+};
