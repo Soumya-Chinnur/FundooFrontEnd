17dfca960f3106c2d231f5b87c8b3cce13a91ec9
Soumya-Chinnur <soumyachinnur@gmail.com>
2020-01-20 14:50:16 +0530

    [Soumya] refactor.modified dashboard component and pushed into remote

--- fundookeep/src/components/dashboard/dashboard.js
+++ fundookeep/src/components/dashboard/dashboard.js
@@ -1,30 +1,26 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
-export default { //standard syntax for instantiating an object that has been defined.
-  name: 'dashboard',
-  components: {},//They help you extend basic HTML elements to encapsulate reusable code
+export default {
+  //standard syntax for instantiating an object that has been defined.
+  name: "dashboard",
+  components: {}, //They help you extend basic HTML elements to encapsulate reusable code
   props: [],
-  data () {
+  data() {
     return {
-      flag: false,
-      
-     }
+      flag: false
+    };
   },
-  computed: {//A computed property is used to declaratively describe a value that depends on other values
-
+  computed: {
+    //A computed property is used to declaratively describe a value that depends on other values
   },
-  mounted () {//mounted() is called after DOM has been mounted so you can access the reactive component, templates, and DOM elements and manipulate them
-
+  mounted() {
+    //mounted() is called after DOM has been mounted so you can access the reactive component, templates, and DOM elements and manipulate them
   },
   methods: {
-    showNavigation()
-    {
-      this.flag = !this.flag ;
-    },
-
+    showNavigation() {
+      this.flag = !this.flag;
+    }
   }
-}
-
-
+};
