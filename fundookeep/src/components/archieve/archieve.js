e750b717435e2a490a269046180c2a430a203a5e
Soumya-Chinnur <soumyachinnur@gmail.com>
2020-01-20 14:51:14 +0530

    [Soumya] refactor.modified archive component and pushed into remote

--- fundookeep/src/components/archieve/archieve.js
+++ fundookeep/src/components/archieve/archieve.js
@@ -1,30 +1,25 @@
-import noteService from "../services/noteService"
+import noteService from "../services/noteService";
 import displayNote from "../displayNote";
 
 export default {
   name: "archieve",
-  components: {displayNote},
+  components: { displayNote },
   props: [],
   data() {
     return {
-      archiveCards:[]
+      archiveCards: []
     };
   },
   computed: {},
   mounted() {
-
-    noteService.getNotes().then(res=>{
-      console.log("fsd",res);
+    noteService.getNotes().then(res => {
+      console.log("fsd", res);
       for (let index = 0; index < res.data.data.data.length; index++) {
         if (res.data.data.data[index].isArchived) {
           this.archiveCards.push(res.data.data.data[index]);
         }
       }
-
-    })
-    
-
-
+    });
   },
   methods: {}
 };
