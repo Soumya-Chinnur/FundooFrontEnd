76d6f9ecfdd55f49814580d44bd78229411bd353
Soumya-Chinnur <soumyachinnur@gmail.com>
2020-01-30 16:19:57 +0530

    [Soumya] refactor.modified trash component and pushed into remote

--- fundookeep/src/components/trash/trash.js
+++ fundookeep/src/components/trash/trash.js
@@ -2,15 +2,15 @@ import noteService from "../services/noteService";
 import displayNote from "../displayNote";
 export default {
   name: "trash",
-  components: { displayNote },
-  props: [],
+  components: { displayNote },//They help you extend basic HTML elements to encapsulate reusable code.
+  props: [],//Props are how you pass data from a parent component down to a child component
   data() {
     return {
       deleteCards:[]
     };
   },
   computed: {},
-  mounted() {
+  mounted() { //mounted() is called after DOM has been mounted so you can access the reactive component, templates, and DOM elements and manipulate them
     noteService.getNotes().then(res => {
       console.log("fsd", res);
       for (let index = 0; index < res.data.data.data.length; index++) {
@@ -21,5 +21,4 @@ export default {
       console.log("sssssssssssssssssssss", this.deleteCards);
     });
   },
-  methods: {}
 };
